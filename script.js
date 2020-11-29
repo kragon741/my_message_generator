e318332cec428fbae800157ffa786bb87492b9dd
Matthew Kohn <kohney319m@gmail.com>
2020-11-29 12:14:15 -0700

    Create encouragement object and populate with title, ability, and action arrays

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ function generateRandomNumber(num) {
   return Math.floor(Math.random() * num);
 }
 
+const encouragement = {
+  title: ['winner', 'sage', 'legend', 'hero', 'wizard', 'master empath', 'tornado', 'trailblazer', 'genius', 'heartthrob'],
+  ability: ['heals', 'transforms', 'inspires', 'amazes', 'helps', 'dazzles', 'respects', 'improves', 'solves', 'saves'],
+  action: ['show them what you are made of', 'make someones day', 'do something nice for yourself', 'pay it forward', 'be kind', 'make someone smile', 'change the world', 'save the day', 'share gratitude', 'take extra special care of yourself']
+}
